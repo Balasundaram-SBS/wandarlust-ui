c04946eec5d523f616bb2e3ea4bfc814c7436b87
Balasundaram-SBS <bala.sbs.somu@gmail.com>
2022-01-08 01:06:23 +0530

    backendurl updated

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,5 +1,5 @@
 
 const port = 4000;
-export const backendUrlUser = `https://wandarlust.herokuapp.com/user`; // /register - POST, /login - POST, /getBookings/:userId - GET
-export const backendUrlPackage = `https://wandarlust.herokuapp.com/user/packages`; // /hotDeals -> GET, /destinations -> GET, 
-export const backendUrlBooking = `https://wandarlust.herokuapp.com/user/book`; // /:userId/:destinationId -> POST, /cancelBooking/:bookingId -> DELETE, /getDetails/:destinationId - GET, 
+export const backendUrlUser = `https://wandarlust-web-service.herokuapp.com/user`; // /register - POST, /login - POST, /getBookings/:userId - GET
+export const backendUrlPackage = `https://wandarlust-web-service.herokuapp.com/user/packages`; // /hotDeals -> GET, /destinations -> GET, 
+export const backendUrlBooking = `https://wandarlust-web-service.herokuapp.com/user/book`; // /:userId/:destinationId -> POST, /cancelBooking/:bookingId -> DELETE, /getDetails/:destinationId - GET, 
